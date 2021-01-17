a6715a72e66165bb35189b670f97303e77740490
Nicu Barbaros <barbarosnicolae93@gmail.com>
2021-01-17 19:12:58 +0200

    👌 Finish  gallery for project 3

---
+++ 3.intersection-observer/src/components/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.scss';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div>WU</div>
+
+      <ul>
+        <li>Cases</li>
+        <li>Services</li>
+        <li>Culture</li>
+        <li>Team</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
