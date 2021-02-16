e4c64d0466460cbfd935d9de167909735d9e1b8c
Nicu Barbaros <barbarosnicolae93@gmail.com>
2021-02-16 21:59:47 +0200

    👌 Lazy load for netlify

--- 3.intersection-observer/src/pages/index.jsx
+++ 3.intersection-observer/src/pages/index.jsx
@@ -1,70 +1,18 @@
 import React from "react";
-import { useEffect } from "react";
-import { useRef } from "react";
-import { imageData } from "../data";
-import LocomotiveScroll from "locomotive-scroll";
-import "../../node_modules/locomotive-scroll/src/locomotive-scroll.scss";
 
 import "../styles/home.scss";
-import GalleryItem from "../components/GalleryItem";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-const Index = () => {
-  const ref = useRef(null);
 
-  useEffect(() => {
-    if (ref) {
-      if (typeof window === "undefined" || !window.document) {
-        return
-      }
-      const scroll = new LocomotiveScroll({
-        el: ref.current,
-        smooth: true,
-        direction: "horizontal",
-        multiplier: 0.5,
-      });
-    }
-  }, []);
-  const images = imageData.map((tupples, index) =>
-    tupples.map((url, elementIndex) => (
-      <GalleryItem
-        key={url}
-        src={url}
-        index={elementIndex}
-        columnOffset={index * 14}
-      />
-    ))
-  );
-  if (typeof window === "undefined" || !window.document) {
-    return null
-  }
+const HomeLazy = React.lazy(() => import("./Home"));
+const Index = () => {
+  const isSSR = typeof window === "undefined";
+
   return (
     <>
-      <div className="top"></div>
-      <Navbar />
-      <div className="main-container">
-        <div className="scroll-container" data-scroll-container ref={ref}>
-          <div className="content">
-            <div className="gallery">
-              {images}
-              <div className="gallery-helper">Scroll to discover more</div>
-              <div className="behind-text fill" data-scroll data-scroll-speed>
-                every body loves a good story
-              </div>
-              <div
-                className="behind-text outline"
-                data-scroll
-                data-scroll-speed
-              >
-                every body loves a good story
-              </div>
-              <div />
-            </div>
-
-            <Footer />
-          </div>
-        </div>
-      </div>
+      {!isSSR && (
+        <React.Suspense fallback={<div />}>
+          <HomeLazy />
+        </React.Suspense>
+      )}
     </>
   );
 };
