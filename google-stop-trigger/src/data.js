9676907357f8384198ac1c6c5a39aa33a33dd11c
Nicu Barbaros <barbarosnicolae93@gmail.com>
2021-02-22 21:35:22 +0200

    👌 Initial setup for scroll trigger

---
+++ google-stop-trigger/src/data.js
@@ -0,0 +1,20 @@
+const photos = [
+  "https://images.unsplash.com/photo-1500081334385-7766153ae227?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=100",
+  "https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1402&q=100",
+
+  "https://images.unsplash.com/photo-1449182210692-60ced46ae304?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2491&q=100",
+
+  "https://images.unsplash.com/photo-1537253500005-0945025bafe6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2700&q=100",
+
+  "https://images.unsplash.com/photo-1504829857797-ddff29c27927?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=100",
+];
+const imageData = [photos, photos, photos, photos];
+
+const points = [
+  [11, 2, 12, 2],
+  [18, 6, 7, 1],
+  [29, 5, 12, 3],
+  [16, 10, 13, 3],
+  [30, 14, 5, 2],
+];
+export { imageData, points };
