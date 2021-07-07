7304cd19ebd6db39c42e6f9da0b7f01a693aa2e0
Nicu Barbaros <barbarosnicolae93@gmail.com>
2021-07-07 09:01:27 +0300

    👌 Make a rough example for tutorial

--- 8.work-page-effect/src/containers/Home.jsx
+++ 8.work-page-effect/src/containers/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
-import photos from "../data";
+import photos, { pageData } from "../data";
 import LocomotiveScroll from "locomotive-scroll";
 import "locomotive-scroll/src/locomotive-scroll.scss";
-
+import cn from 'classnames';
+ 
 import imagesLoaded from "imagesloaded";
 
 import "../styles/home.scss";
-
 
 const preloadImages = (selector) => {
   return new Promise((resolve) => {
@@ -22,15 +22,14 @@ const preloadImages = (selector) => {
 
 const Home = () => {
   const ref = useRef(null);
-  const leftColumnRef = useRef(null);
-  const rightColumnRef = useRef(null);
-  const middleColumnRef = useRef(null);
+
+  const [rotationPosition, setRotation] = useState(new Array(pageData.length).fill(0));
+  const [activeIndex , setActiveIndex ] = useState(-1);
   const scroll = useRef({
     cache: 0,
     current: 0,
   });
   useEffect(() => {
-   
     // const scrollElement = new LocomotiveScroll({
     //   el: ref.current,
     //   smooth: true,
@@ -48,7 +47,6 @@ const Home = () => {
     //   scroll.current.current = obj.scroll.y;
     //   const distance = scroll.current.current - scroll.current.cache;
     //   scroll.current.cache = scroll.current.current;
-
     //   leftColumnRef.current.style.transform = `skewY(${clamp(
     //     distance,
     //     -10,
@@ -65,23 +63,59 @@ const Home = () => {
     //     10
     //   )}deg)`;
     // });
-
     // // Preload images
     // Promise.all([preloadImages(".grid-item-media")]).then(() => {
     //   scrollElement.update();
     // });
   }, []);
 
+
+  const handleSetRotation = (itemIndex)=>{
+    // Random between -7 and 7
+    const newRotation =(Math.random() * 7) * (Math.round(Math.random()) ? 1 : -1);
+
+    const tempState = [...rotationPosition];
+    tempState[itemIndex] = newRotation;
+    setRotation(tempState);
+    setActiveIndex(itemIndex);
+  }
+
+  console.log(rotationPosition);
+
   return (
     <>
-
       <div
         className="main-container"
         id="main-container"
-        data-scroll-container
-        ref={ref}
+        // data-scroll-container
       >
-        
+        <div className="title-container"
+        // ref={ref}
+        // data-scroll-container
+        >
+          {pageData.map(({ title }, index) => (
+            <div
+              className="title-item"
+              key={title}
+              onMouseEnter={() => handleSetRotation(index)}
+              onMouseLeave={() => setActiveIndex(-1)}
+            >
+              <h1>{title}</h1>
+            </div>  
+          ))}
+        </div>
+        <div className="image-container">
+          {pageData.map(({ url }, index) => (
+            <img
+              className={cn({ active: activeIndex === index })}
+              style={{
+                transform: activeIndex === index   ?`scale(1.1) rotate(${rotationPosition[index]}deg)`: `rotate(${rotationPosition[index]}deg)`
+              }}
+              key={url}
+              src={url}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- 8.work-page-effect/src/data.js
+++ 8.work-page-effect/src/data.js
@@ -1,64 +1,39 @@
 
-const photos = [
+export const pageData = [
   {
-    url: "https://images.unsplash.com/photo-1622901120958-ae569882629c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 1",
+    url: "https://images.unsplash.com/photo-1593939535589-8356e421b3cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1923&q=80",
+    title: "Chanel",
   },
   {
-    url: "https://images.unsplash.com/photo-1476900543704-4312b78632f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80",
-    description: "Block 2",
+    url: "https://images.unsplash.com/photo-1618436917352-cd3d11ea4d15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80",
+    title: "Dior",
   },
   {
-    url: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 3",
+    url: "https://images.unsplash.com/photo-1550270428-27e40d58bb7d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    title: "Hermès",
   },
   {
-    url: "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80",
-    description: "Block 4",
+    url: "https://images.unsplash.com/photo-1600612253971-422e7f7faeb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80",
+    title: "Yves Saint Laurent",
   },
   {
-    url: "https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=930&q=80",
-    description: "Block 5",
+    url: "https://images.unsplash.com/photo-1547996160-81dfa63595aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
+    title: "Rolex",
   },
   {
-    url: "https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80",
-    description: "Block 6",
+    url: "https://images.unsplash.com/photo-1600704514457-fcd7f8c05a31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    title: "ZARA",
   },
   {
-    url: "https://images.unsplash.com/photo-1534366428-e54c1db0bed4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80",
-    description: "Block 7",
+    url: "https://images.unsplash.com/photo-1598452963314-b09f397a5c48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1401&q=80",
+    title: "Gucci",
   },
   {
-    url: "https://images.unsplash.com/photo-1527631746610-bca00a040d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 8",
+    url: "https://images.unsplash.com/photo-1564845301949-6eae59181233?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
+    title: "Louis Vuitton",
   },
   {
-    url: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 9",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1526315251174-de0107e28fb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 10",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80",
-    description: "Block 11",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1534366428-e54c1db0bed4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80",
-    description: "Block 12",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1527631746610-bca00a040d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 13",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 14",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1526315251174-de0107e28fb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-    description: "Block 15",
-  },
+    url: "https://images.unsplash.com/photo-1587701711862-d52756eacc6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "Supreme",
+  }
 ];
-export default photos;
