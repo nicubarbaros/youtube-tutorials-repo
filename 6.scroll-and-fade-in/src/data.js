09bac92652cccc61bf482a024698f19cd1e55edb
Nicu Barbaros <nicholas.barbaros@gmail.com>
2023-05-13 23:09:37 +0300

    👌 Remove old project

--- 6.scroll-and-fade-in/src/data.js
+++
@@ -1,5 +0,0 @@
-const photos = [
-  "https://images.unsplash.com/photo-1598838073192-05c942ede858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=100",
-  "https://images.unsplash.com/photo-1552248524-10d9a7e4841c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=881&q=100",
-];
-export default photos;
